df9aacc9395efdc4b144a3d8b871908c8247f38f
KiomiH <45368652+KiomiH@users.noreply.github.com>
2018-11-26 13:29:47 -0500

    Update and rename README.md to sketch.js

--- README.md
+++
@@ -1,1 +0,0 @@
-# floppy-ear-bunny2
---
+++ sketch.js
@@ -0,0 +1,59 @@
+function setup() {
+	createCanvas(1000, 600);
+	background(210, 50, 170);
+
+}
+
+function draw() {
+	fill(255);
+	stroke(0);
+	ellipse(410, 200, 95, 270);
+	fill(250, 150, 170);
+	stroke(250,150,170);
+	ellipse(410, 250, 50, 200);
+	fill(255);
+	stroke(0);
+	ellipse(590, 200, 95, 270);
+	fill(250, 150, 170);
+	stroke(250,150,170);
+	ellipse(590, 250, 50, 200);
+	fill(255);
+	stroke(0);
+	ellipse(500, 400, 350, 290);
+	fill(0);
+	stroke(0);
+	ellipse(460, 380, 25, 45);
+	fill(0);
+	stroke(0);
+	ellipse(540, 380, 25, 45);
+	fill(255);
+	stroke(0);
+	rect(470, 465, 30, 50);
+	fill(255);
+	stroke(0);
+	rect(500, 465, 30, 50);
+	fill(255);
+	stroke(255);
+	rect(470, 460, 70, 20);
+	fill(255);
+	stroke(0);
+	curve(485, 100, 500, 450, 450, 500, 390, 400);
+	fill(255);
+	stroke(0);
+	curve(485, 100, 500, 450, 550, 500, 610, 400);
+	fill(250, 150, 170);
+	stroke(0);
+	ellipse(500, 440, 80, 55);
+	fill(255);
+	stroke(0);
+	ellipse(455, 370, 8, 17);
+	fill(255);
+	stroke(0);
+	ellipse(535, 370, 8, 17);
+	fill(210,50,170);
+	stroke(210,50,170);
+	rect(540, 60, 95, 65);
+	fill(255);
+	stroke(0);
+	triangle(550, 125, 640, 230, 660, 125); 
+}
